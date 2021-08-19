49dae62c8a0b89f383175bf97f689604db0735b0
Daijiang Li <dli1@ufl.edu>
2021-08-19 10:38:20 -0500

    update r

--- R/build.R
+++ R/build.R
@@ -23,3 +23,16 @@ if(length(hfiles)){
 }
 
 
+# rmarkdown posts syntax highlighting
+# the default html generated has `class="r"`, 
+# we need `class=language-r` to make the highlighting work
+
+hfiles2 = list.files("content/post", "*.html$", recursive = T, full.names = T)
+
+if(length(hfiles2)){
+  for(i in 1:length(hfiles2)){
+    xfun::gsub_files(hfiles2[i], pattern = 'class="r"', 
+                     replacement = 'class="language-r"')
+  }
+}
+
